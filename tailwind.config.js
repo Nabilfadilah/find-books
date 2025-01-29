2de1f7415f6f0a85059c3329af511e45f1322cef
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-29 15:59:48 +0700

    Menambah file vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{vue,js,ts,jsx,tsx}', // termasuk file .vue, .js, .ts, dll
   ],
   theme: {
     extend: {},
