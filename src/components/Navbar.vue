158e2fdb27f823653f02508eb0289df0a882bcbe
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-29 16:30:16 +0700

    Membuat component navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="p-4 h-16 border-b bg-white/90 shadow-md flex justify-between items-center">
+    <h1 class="text-2xl font-bold text-zinc-900">Temukan Buku</h1>
+    <div class="space-x-4">
+      <button class="px-4 py-2 bg-zinc-900 text-white rounded-md hover:bg-zinc-700">Home</button>
+      <button class="px-4 py-2 bg-zinc-900 text-white rounded-md hover:bg-zinc-700">Favorites</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
